491e649a5a5d5f31f520f9c1f1988165d3807e85
tolmamal <tolmamal@gmail.com>
2020-11-23 14:54:23 +0200

    shop navigator

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import { useSelector } from "react-redux";
 
 const ProductsOverviewScreen = props => {
+    const products = useSelector(state => state.products.availableProducts);
+
     return (
-        <FlatList />
+        <FlatList
+            data={products}
+            keyExtractor={item => item.id}
+            renderItem={itemData => <Text>{itemData.item.title}</Text>}
+        />
     );
 };
 
