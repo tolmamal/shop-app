847a3d616d4fc795e1c051d0f884ca53c94afc39
tolmamal <tolmamal@gmail.com>
2020-11-23 14:38:51 +0200

    products reducer

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return (
+        <FlatList />
+    );
+};
+
+
+export default ProductsOverviewScreen;
