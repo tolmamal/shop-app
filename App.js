f710c586dc60119dd671600f5e021db922839275
tolmamal <tolmamal@gmail.com>
2020-12-03 13:37:35 +0200

    expo-font

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
