8546f13a5224d7ee3a256151e7477bfcd7f599ed
tolmamal <tolmamal@gmail.com>
2020-12-03 17:00:46 +0200

    cartReducer

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
-
-import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
 
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+
 const rootReducer = combineReducers({
-  products: productsReducer,
+    products: productsReducer,
+    cart: cartReducer
 });
 
 const store = createStore(rootReducer);
